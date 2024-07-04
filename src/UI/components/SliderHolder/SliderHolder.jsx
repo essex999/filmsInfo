c2bb5b63d4dae5e9d82641d82bc804bdc0380e89
Essex X <120477833+essex999@users.noreply.github.com>
2024-07-04 12:40:42 +0000

    sliderChanges

--- src/UI/components/SliderHolder/SliderHolder.jsx
+++ src/UI/components/SliderHolder/SliderHolder.jsx
@@ -1,52 +1,61 @@
-import React from 'react'
-import Slider from 'react-slick'
-import 'slick-carousel/slick/slick-theme.css'
-import 'slick-carousel/slick/slick.css'
-import { NextArrow } from './Arrows/NextArrow'
-import { PrevArrow } from './Arrows/PrevArrow'
-import { SliderElement } from './SliderElement/SliderElement'
-import styles from './SliderHolder.module.scss'
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
+import { NextArrow } from "./Arrows/NextArrow";
+import { PrevArrow } from "./Arrows/PrevArrow";
+import { SliderElement } from "./SliderElement/SliderElement";
+import styles from "./SliderHolder.module.scss";
+import { useGetMovies } from "../../../query/moviesApi/getSliderTopMovies";
+
 export default function SliderHolder() {
-	const mockData = [
-		'John Doe',
-		'Jane Smith',
-		'Michael Johnson',
-		'Emily Brown',
-		'David Williams',
-		'Jessica Davis',
-		'Daniel Martinez',
-		'Sarah Wilson',
-		'Christopher Garcia',
-		'Lisa Miller',
-	]
+  const { data, refetch } = useGetMovies();
+  console.log(data);
+  const mockData = [
+    "John Doe",
+    "Jane Smith",
+    "Michael Johnson",
+    "Emily Brown",
+    "David Williams",
+    "Jessica Davis",
+    "Daniel Martinez",
+    "Sarah Wilson",
+    "Christopher Garcia",
+    "Lisa Miller",
+  ];
 
-	let settings = {
-		dots: true,
-		infinite: true,
+  let settings = {
+    dots: true,
+    infinite: true,
 
-		slidesToShow: 8,
-		slidesToScroll: 2,
-		autoplay: true,
-		speed: 400, //milliseconds
-		autoplaySpeed: 5500, //milliseconds
-		nextArrow: <NextArrow />, // custom arrows
-		prevArrow: <PrevArrow />,
-	}
-	return (
-		<div className={styles.sliderWrapper}>
-			<div className={`${'slider-container'} ${styles.sliderContainer}`}>
-				<Slider {...settings} className={styles.slider}>
-					{mockData.map((el, index) => (
-						<div key={index}>
-							<div className={styles.sliderElement}>
-								<div className={styles.sliderContent}>
-									<SliderElement />
-								</div>
-							</div>
-						</div>
-					))}
-				</Slider>
-			</div>
-		</div>
-	)
+    slidesToShow: 8,
+    slidesToScroll: 2,
+    autoplay: true,
+    speed: 400, //milliseconds
+    autoplaySpeed: 5500, //milliseconds
+    nextArrow: <NextArrow />, // custom arrows
+    prevArrow: <PrevArrow />,
+  };
+  return (
+    <div className={styles.sliderWrapper}>
+      <div className={`${"slider-container"} ${styles.sliderContainer}`}>
+        <Slider {...settings} className={styles.slider}>
+          {mockData.map((el, index) => (
+            <div key={index}>
+              <div className={styles.sliderElement}>
+                <div className={styles.sliderContent}>
+                  <SliderElement />
+                </div>
+              </div>
+            </div>
+          ))}
+        </Slider>
+      </div>
+      <button
+        onClick={() => {
+          refetch();
+        }}
+      />
+    </div>
+  );
 }
---
+++ src/query/moviesApi/getSliderTopMovies.js
@@ -0,0 +1,32 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { API_KEY } from "../../consts";
+
+const BASE_URL = "https://api.kinopoisk.dev/";
+
+export const useGetMovies = () => {
+  const { data, isError, isSuccess, refetch } = useQuery({
+    queryKey: ["topMoviesData"],
+    enabled: false,
+    queryFn: async () => {
+      const response = await axios.get(`${BASE_URL}v1.4/movie`, {
+        params: {
+          year: "2024",
+          type: "movie",
+          limit: 34,
+          "rating.imdb": "8.2 - 10",
+          movieLength: "70 - 200",
+        },
+        headers: { "X-API-KEY": API_KEY },
+      });
+      return response;
+    },
+  });
+
+  return {
+    data,
+    isError,
+    refetch,
+    isSuccess,
+  };
+};
