b0c440a6e9ed4197a313886c9f1d8ca73c0e9c38
Essex X <120477833+essex999@users.noreply.github.com>
2024-07-26 14:43:34 +0000

    zzx

--- src/UI/components/SearchBar/SearchBar.js
+++ src/UI/components/SearchBar/SearchBar.js
@@ -1,105 +1,111 @@
-import { useEffect, useState } from 'react'
-import IconArrowDropDownLine from '../../../assets/IconComponents/arrowElectronIcon.js'
-import { categories } from '../../../consts.js'
+import { useEffect, useState } from "react";
+import IconArrowDropDownLine from "../../../assets/IconComponents/arrowElectronIcon.js";
+import { categories } from "../../../consts.js";
 
-import { useDispatch, useSelector } from 'react-redux'
-import { useLocation, useNavigate } from 'react-router-dom'
-import SearchIcon from '../../../assets/IconComponents/searchIcon.js'
-import { setSearchTextContent } from '../../../redux/slicers/searchMovieParamsSlicer.js'
-import { BarContentPopUp } from '../BarContentPopUp/BarContentPopUp.jsx'
-import styles from './SearchBar.module.scss'
+import { useDispatch, useSelector } from "react-redux";
+import { useLocation, useNavigate } from "react-router-dom";
+import SearchIcon from "../../../assets/IconComponents/searchIcon.js";
+import { setSearchTextContent } from "../../../redux/slicers/searchMovieParamsSlicer.js";
+import { BarContentPopUp } from "../BarContentPopUp/BarContentPopUp.jsx";
+import styles from "./SearchBar.module.scss";
 export const SearchBar = ({ setTrigger }) => {
-	const navigate = useNavigate()
-	const dispatch = useDispatch()
-	const [isToggle, setToggle] = useState(true)
-	const [onFocusData, setOnFocusData] = useState({})
-	const placeholderValue = 'Поиск Фильмов и Сериалов'
-	const [typeNumber, setTypeNumber] = useState(0)
-	const [isNavSubMenu, setIsNavSubMenu] = useState(false)
-	const [textcontent, setTextContent] = useState('')
-	const pathname = useLocation().pathname
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [isToggle, setToggle] = useState(true);
+  const [onFocusData, setOnFocusData] = useState({});
+  const placeholderValue = "Поиск Фильмов и Сериалов";
+  const [typeNumber, setTypeNumber] = useState(0);
+  const [isNavSubMenu, setIsNavSubMenu] = useState(false);
+  const [textcontent, setTextContent] = useState("");
+  const pathname = useLocation().pathname;
 
-	const searchPage = pathname.split('/').find(el => el === 'search')
-	const inputTextContent = useSelector(
-		state => state.searchParams.searchTextContent
-	)
+  const searchPage = pathname.split("/").find((el) => el === "search");
+  const inputTextContent = useSelector(
+    (state) => state.searchParams.searchTextContent,
+  );
 
-	useEffect(() => {
-		setTextContent(inputTextContent)
-	}, [])
-	return (
-		<div
-			className={styles.HeaderMain}
-			onMouseLeave={e => {
-				setIsNavSubMenu(false)
-			}}
-		>
-			<div className={styles.HeaderNav}>
-				<div className={styles.HeaderButtonsGroup}>
-					{categories.map((content, index) => (
-						<div
-							className={styles.CategoryButton}
-							onMouseOver={e => {
-								setOnFocusData({
-									focused: true,
-									index: index,
-								}) //arrow animation event
-								setTypeNumber(index + 1)
-								setIsNavSubMenu(true) //searchBarMenu popUp event
-							}}
-							onMouseLeave={e => {
-								setOnFocusData({ focused: false, index: index }) //arrow animation event
-							}}
-							key={index}
-						>
-							{content}
-							<IconArrowDropDownLine index={index} focus={onFocusData} />
-						</div>
-					))}
-				</div>
+  useEffect(() => {
+    setTextContent(inputTextContent);
+  }, []);
 
-				<form
-					className={styles.form}
-					onSubmit={e => {
-						e.preventDefault()
+  const enabled = () => {
+    if (textcontent.length > 0) {
+      return false;
+    } else return true;
+  };
+  return (
+    <div
+      className={styles.HeaderMain}
+      onMouseLeave={(e) => {
+        setIsNavSubMenu(false);
+      }}
+    >
+      <div className={styles.HeaderNav}>
+        <div className={styles.HeaderButtonsGroup}>
+          {categories.map((content, index) => (
+            <div
+              className={styles.CategoryButton}
+              onMouseOver={(e) => {
+                setOnFocusData({
+                  focused: true,
+                  index: index,
+                }); //arrow animation event
+                setTypeNumber(index + 1);
+                setIsNavSubMenu(true); //searchBarMenu popUp event
+              }}
+              onMouseLeave={(e) => {
+                setOnFocusData({ focused: false, index: index }); //arrow animation event
+              }}
+              key={index}
+            >
+              {content}
+              <IconArrowDropDownLine index={index} focus={onFocusData} />
+            </div>
+          ))}
+        </div>
 
-						dispatch(setSearchTextContent(textcontent))
+        <form
+          className={styles.form}
+          onSubmit={(e) => {
+            e.preventDefault();
 
-						if (!searchPage) {
-							navigate('/search/1')
-						}
+            dispatch(setSearchTextContent(textcontent));
 
-						if (setTrigger) {
-							setTrigger(Math.random())
-						}
-					}}
-				>
-					<input
-						className={styles.HeaderInput}
-						value={textcontent}
-						onChange={e => {
-							setTextContent(e.target.value)
-						}}
-						onFocus={() => {
-							setToggle(!isToggle)
-						}}
-						onBlur={() => {
-							setToggle(!isToggle)
-						}}
-						type='text'
-						placeholder={isToggle ? placeholderValue : ''}
-					/>
-					<button type='submit'>
-						<SearchIcon className={styles.searchIcon} />
-					</button>
-				</form>
-			</div>
-			<BarContentPopUp
-				show={isNavSubMenu}
-				content={onFocusData}
-				typeNumber={typeNumber}
-				showToggle={setIsNavSubMenu}
-			/>
-		</div>
-	)
-}
+            if (!searchPage) {
+              navigate("/search/1");
+            }
+
+            if (setTrigger) {
+              setTrigger(Math.random());
+            }
+          }}
+        >
+          <input
+            className={styles.HeaderInput}
+            value={textcontent}
+            onChange={(e) => {
+              setTextContent(e.target.value);
+            }}
+            onFocus={() => {
+              setToggle(!isToggle);
+            }}
+            onBlur={() => {
+              setToggle(!isToggle);
+            }}
+            type="text"
+            placeholder={isToggle ? placeholderValue : ""}
+          />
+          <button disabled={enabled()} type="submit">
+            <SearchIcon className={styles.searchIcon} />
+          </button>
+        </form>
+      </div>
+      <BarContentPopUp
+        show={isNavSubMenu}
+        content={onFocusData}
+        typeNumber={typeNumber}
+        showToggle={setIsNavSubMenu}
+      />
+    </div>
+  );
+};
--- src/query/moviesApi/useSearchMovie.js
+++ src/query/moviesApi/useSearchMovie.js
@@ -1,29 +1,28 @@
-import { useQuery, useQueryClient } from '@tanstack/react-query'
-import axios from 'axios'
-import { API_KEY } from '../../consts'
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { API_KEY } from "../../consts";
 
-const BASE_URL = 'https://api.kinopoisk.dev/'
+const BASE_URL = "https://api.kinopoisk.dev/";
 
-export const useSearchMovies = params => {
-	const queryClient = useQueryClient()
-	const { data, isError, refetch, isLoading, isFetching } = useQuery({
-		queryKey: ['searchedMoviesData', params],
-		enabled: false,
-		retry: 0,
-		queryFn: async () => {
-			const response = await axios.get(`${BASE_URL}v1.4/movie/search`, {
-				params: { ...params, limit: 16 },
-				headers: { 'X-API-KEY': API_KEY },
-			})
-			return response.data
-		},
-	})
+export const useSearchMovies = (params) => {
+  const { data, isError, refetch, isLoading, isFetching } = useQuery({
+    queryKey: ["searchedMoviesData", params],
+    enabled: false,
+    retry: 0,
+    queryFn: async () => {
+      const response = await axios.get(`${BASE_URL}v1.4/movie/search`, {
+        params: { ...params, limit: 16 },
+        headers: { "X-API-KEY": API_KEY },
+      });
+      return response.data;
+    },
+  });
 
-	return {
-		data,
-		isError,
-		refetch,
-		isLoading,
-		isFetching,
-	}
-}
+  return {
+    data,
+    isError,
+    refetch,
+    isLoading,
+    isFetching,
+  };
+};
